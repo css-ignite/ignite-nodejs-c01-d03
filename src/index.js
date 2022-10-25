2c0c492f880710c4835d02d0c0cc35f851da2505
claudneysessa <claudneysartisessa@gmail.com>
2022-10-25 00:31:57 -0300

    configurando o debug inLine no VsCode e o ESLint

--- src/index.js
+++ src/index.js
@@ -1,104 +1,103 @@
-const express = require("express");
+const express = require('express')
 
-const { v4: uuid } = require("uuid");
+const { v4: uuid } = require('uuid')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-const repositories = [];
+const repositories = []
 
-function middlewareFindRepositoryByIndex(request, response, next){
-  const { id } = request.params;
+function middlewareFindRepositoryByIndex (request, response, next) {
+  const { id } = request.params
 
   const repositoryIndex = repositories.findIndex(
     (repository) => repository.id === id
-  );
+  )
 
   if (repositoryIndex < 0) {
-    return response.status(404).json({ 
-      error: "Repository not found." 
-    });
+    return response.status(404).json({
+      error: 'Repository not found.'
+    })
   }
 
-  request.repositoryIndex = repositoryIndex;
+  request.repositoryIndex = repositoryIndex
 
-  return next();
-
+  return next()
 }
 
 /*
   (GET) /repositories
 */
 
-app.get("/repositories", (request, response) => {
-  return response.json(repositories);
-});
+app.get('/repositories', (request, response) => {
+  return response.json(repositories)
+})
 
 /*
   (POST) /repositories
 */
 
-app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body;
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body
 
   const repository = {
     id: uuid(),
     title,
     url,
     techs,
-    likes: 0,
-  };
+    likes: 0
+  }
 
-  repositories.push(repository);
+  repositories.push(repository)
 
-  return response.json(repository);
-});
+  return response.json(repository)
+})
 
 /*
   (PUT) /repositories/:id
 */
 
-app.put("/repositories/:id", middlewareFindRepositoryByIndex, (request, response) => {
-  const { id } = request.params;
-  const { title, url, techs } = request.body;
-  const { repositoryIndex } = request;
+app.put('/repositories/:id', middlewareFindRepositoryByIndex, (request, response) => {
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  const { repositoryIndex } = request
 
   const repository = {
     id,
     title,
     url,
     techs,
-    likes: repositories[repositoryIndex].likes,
-  };
+    likes: repositories[repositoryIndex].likes
+  }
 
-  repositories[repositoryIndex] = repository;
+  repositories[repositoryIndex] = repository
 
-  return response.json(repository);
-});
+  return response.json(repository)
+})
 
 /*
   (DELETE) /repositories/:id
 */
 
-app.delete("/repositories/:id", middlewareFindRepositoryByIndex, (request, response) => {
-  const { repositoryIndex } = request;
+app.delete('/repositories/:id', middlewareFindRepositoryByIndex, (request, response) => {
+  const { repositoryIndex } = request
 
-  repositories.splice(repositoryIndex, 1);
+  repositories.splice(repositoryIndex, 1)
 
-  return response.status(204).send();
-});
+  return response.status(204).send()
+})
 
 /*
   (PATCH) /repositories/:id/like
 */
 
-app.post("/repositories/:id/like", middlewareFindRepositoryByIndex, (request, response) => {
-  const { repositoryIndex } = request;
+app.post('/repositories/:id/like', middlewareFindRepositoryByIndex, (request, response) => {
+  const { repositoryIndex } = request
 
-  const likes = ++repositories[repositoryIndex].likes;
+  const likes = ++repositories[repositoryIndex].likes
 
-  return response.json({ likes: likes });
-});
+  return response.json({ likes })
+})
 
-module.exports = app;
+module.exports = app
